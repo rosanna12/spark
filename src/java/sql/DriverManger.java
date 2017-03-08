6190ff16ff7d8499392910ea3a616b50dabfcde9
rosanna12 <sweetydavis12@gmail.com>
2017-03-09 04:50:22 +0530

    Add files via upload

---
+++ src/java/sql/DriverManger.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java.sql;
+
+/**
+ *
+ * @author sweety
+ */
+public class DriverManger {
+
+    public static Connection getConnection(String jdbcoraclethinlocalhost1521XE, String system, String oracle) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
